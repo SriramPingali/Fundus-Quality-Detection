e2c6714d687e9f584fc339de52dbdc1ab0ef22d3
SriramPingali <sriram26354@gmail.com>
2020-06-21 09:36:57 +0530

    Add helper functions.
    Put the helper functions in different files.

---
+++ data.py
@@ -0,0 +1,60 @@
+import torch
+import numpy as np
+from PIL import Image
+from os import listdir
+from os.path import isfile, join
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+
+class dataset(Dataset):
+    def __init__(self, image_root):
+        self.image_root = image_root
+        self.data_len = 0
+        self.labels = listdir(image_root)
+#         self.labels = ['Bad', 'Good']
+        self.data = {}
+        self.transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+#             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+        
+        for label in self.labels:
+            for f in listdir('./DRIMDB/' + label):
+                if isfile(join('./DRIMDB/' + label, f)):
+                    self.data[join('./DRIMDB/', label, f)] = label
+                    self.data_len += 1
+
+    def __len__(self):
+        return(self.data_len)
+    
+    def __getitem__(self, idx):
+        img_path = list(self.data.keys())[idx]
+        label = list(self.data.values())[idx]
+        img = Image.open(img_path)
+        img = img.convert('RGB')
+        img = self.transform(img)
+        return(img, label)
+
+def dataloader(dataset, batch_size, validation_split, shuffle_dataset):
+    random_seed= 42
+
+    # Creating data indices for training and validation splits:
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(validation_split * dataset_size))
+    if shuffle_dataset :
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+
+    # Creating PT data samplers and loaders:
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, 
+                                               sampler=train_sampler)
+    val_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                                    sampler=valid_sampler)
+    return(train_loader, val_loader)
---
+++ utils.py
@@ -0,0 +1,39 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+def encode(label, device):
+    rep = torch.zeros(3, dtype = torch.long).to(device)
+    if label == 'Good':
+        rep[0] = 1
+    elif label == 'Bad':
+        rep[1] = 1
+    elif label == 'Outlier':
+        rep[2] = 1
+    return(rep)
+
+def encode_batch(batch, device):
+    encode_tensor = None
+    for i, label in enumerate(batch):
+        rep = encode(label, device)
+        if encode_tensor == None:
+            encode_tensor_size = list(rep.size())
+            encode_tensor_size.insert(0, len(batch))
+            encode_tensor = torch.empty(*encode_tensor_size, dtype=rep.dtype, device=rep.device)
+        encode_tensor[i, :] = rep
+    return(encode_tensor)
+
+def imshow(img):
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+def visualize_results(loss_b, acc_b):
+	plt.figure(figsize = (18, 6))
+
+	plt.subplot(1, 2, 1)
+	plt.ylabel('loss')
+	plt.plot(loss_b)
+
+	plt.subplot(1, 2, 2)
+	plt.plot(acc_b)
